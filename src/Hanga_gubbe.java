ed91221c6b6ee285b43880ef1eb93ee6c0d7e7ca
AdamLagerqvist <adam.lagerqvist@elev.ga.ntig.se>
2021-01-19 13:40:04 +0100

    Börjat på gissnings systemet
    Coommit inneholl:
    
    En ny instans hänga gubbe + några class vaiabler + 2 nya funktioner.
    
    Instanser:
    
    (Hanga_gubbe) En ny instans som jag använder som game loop
    
    Funktioner:
    
    (Rätt_Eller_Fel) Ny funktion som kollar om ens gissning är rätt eller fel lägger till rätt eller fel och skickar data till print funktionen för att den ska kunna skriva ut ordet rätt.
    
    (Print) Ny funktion för att skriva ut ordet/hur många bokstäver man har rätt och hur många fel man har.

--- src/Hanga_gubbe.java
+++ src/Hanga_gubbe.java
@@ -3,18 +3,80 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Hanga_gubbe {
-    public static void main(String[] args){
+public class Hanga_gubbe{
+    String Hang_ord;
+    int Fel = 0;
+    int Rätt = 0;
+    ArrayList<Character> U = new ArrayList<Character>();
+    boolean Ub = true;
+
+    /*
+    Är min game loop
+     */
+    Hanga_gubbe() {
         ArrayList<String> Ord = new ArrayList<String>();
         Ord = from_file("Ord");
         // write_S_ArrayList(Ord);
-        Rand_List(Ord);
+        Hang_ord = (Rand_List(Ord));
+        print();
     }
-    public static void Rand_List(ArrayList<String> list) {
-        System.out.println(list.get((int) (Math.random() * list.size())));
+
+    public static void main(String[] args){
+        Hanga_gubbe instans = new Hanga_gubbe();
+        instans.Rätt_Eller_Fel('O');
+        instans.Rätt_Eller_Fel('q');
     }
-    public static ArrayList<String> from_file(String input_file){
+
+    /*
+    Kollar om en String/gissning finns i ordet 1 eller flera gånger
+    startar också Utritnings systemet
+    */
+    public void Rätt_Eller_Fel(char S) {
+        boolean Om_Fel = true;
+        for (int i = 0;i < Hang_ord.length();i++) {
+            if (Character.toUpperCase(S) == Character.toUpperCase(Hang_ord.charAt(i))) {
+                U.set(i, S);
+                Rätt++;
+                Om_Fel = false;
+            }
+        }
+        if (Om_Fel){
+            Fel++;
+        }
+        print();
+    }
+
+    /*
+    Printar ut ordet + antalet fel du har (fixar gui'n)
+    */
+    public void print(){
+        if(Ub) {
+            for (int i = 0; i < Hang_ord.length(); i++) {
+                U.add('_');
+                Ub = false;
+            }
+        }
+        System.out.println("\n\n\n\n\n\n\n");
+        for (int i=0; i < U.size(); i++) {
+            System.out.print(U.get(i));
+        }
+        System.out.println("\nRätt: " + Rätt + "\nFel: " + Fel);
+    }
+
+    /*
+    Funktionen ger ett svar för ett random index från en in skickad ArrayList
+     */
+    public String Rand_List(ArrayList<String> list) {
+        return list.get((int) (Math.random() * list.size()));
+    }
+
+    /*
+    Funktion för att skriva ord till en ArrayList från en fill där inputten är filnamnet
+     */
+    public ArrayList<String> from_file(String input_file){
         Scanner in = null;
+        // Letar efter filen med filnamnet
+        // Om den ite hittar filen avbryts programmet soch meddelandet "Filen Hittades inte" skickas
         {
             try {
                 in = new Scanner(new File(input_file));
@@ -24,17 +86,21 @@ public class Hanga_gubbe {
                 System.exit(0);
             }
         }
+        // loop för inmatningen av ord i ArrayListen
         ArrayList<String> Ord = new ArrayList<String>();
         while (in.hasNext()) {
             Ord.add(in.nextLine());
         }
+        // skickar ut listan full med ord
         return Ord;
     }
-    public static void write_S_ArrayList (ArrayList<String> sring_arr){
-        int i = 0;
-        while (i < sring_arr.size()){
+
+    /*
+    Funktion för utskrift av ArrayLists
+     */
+    public void write_S_ArrayList (ArrayList<String> sring_arr){
+        for (int i = 0; i < sring_arr.size(); i++){
             System.out.println(sring_arr.get(i));
-            i++;
         }
     }
 }
